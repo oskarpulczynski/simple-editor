b1d03e29be1ba04371a94e6e9e2dfb6ab0d0d6e0
Oskar <o.pulczynski@gmail.com>
2024-09-23 07:40:41 +0200

    PWA Starter and added new button witch check function

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const saveButton = document.querySelector(".button__save--js");
 const loadButton = document.querySelector(".button__load--js");
+const checkButton = document.querySelector(".button__check--js");
 const myEditor = document.querySelector(".my_editor--js");
 
 const saveFunction = e => {
@@ -10,5 +11,23 @@ const loadFunction = e => {
 	myEditor.value = localStorage.getItem("message");
 };
 
+let color = "green";
+
+const checkFunction = e => {
+	const textLength = myEditor.value.length;
+	myEditor.classList.remove("text-gray-900", "dark:text-white");
+	myEditor.classList.remove(`text-${color}-600`, `dark:text-${color}-600`);
+	if (textLength <= 10) {
+		color = "red";
+	} else if (textLength > 10 && textLength <= 20) {
+		color = "blue";
+	} else {
+		color = "green";
+	}
+
+	myEditor.classList.add(`text-${color}-600`, `dark:text-${color}-600`);
+};
+
 saveButton.addEventListener("click", saveFunction);
 loadButton.addEventListener("click", loadFunction);
+checkButton.addEventListener("click", checkFunction);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+	safelist: ["text-red-600", "text-blue-600", "text-green-600"],
+};
